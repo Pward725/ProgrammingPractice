87dbd10c76d63c83bd81703b584545222470311b
pward725 <phillip.j.ward725@gmail.com>
2022-04-01 09:32:40 -0600

    Refactor code structure

---
+++ ProgrammingII/Assignment 1/C2A1E1/C2A1E1_Macros.h
@@ -0,0 +1,18 @@
+// 
+// Phillip Ward U09339367
+// Phillip.Ward@seagate.com
+// C/C++ Programming II
+// 164440 Ray Mitchell
+// 03/29/2022
+// C2A1E1_Macros.h
+// Win10
+// g++ 11.2.0
+// 
+// This is a header file containing some function like macros
+//
+#ifndef C2A1E1_MACROS_H
+#define C2A1E1_MACROS_H
+#define Product(A, B) ((A)*(B))
+#define Negate(A) (-(A))
+#define Elements(A) (sizeof(A)/sizeof((A)[0]))
+#endif
