6e65b8cd5a5f383d9e8a57b154ed0d1f313783ab
pward725 <phillip.j.ward725@gmail.com>
2022-04-02 16:58:28 -0600

    Assignment 3 done

--- ProgrammingII/Assignment 1/C2A1E3/C2A1E3_FindFirstInt.c
+++ ProgrammingII/Assignment 1/C2A1E3/C2A1E3_FindFirstInt.c
@@ -4,22 +4,25 @@
 // C/C++ Programming I
 // 164440 Ray Mitchell
 // 04/01/2022
-// C2A1E2_main.c
+// C2A1E3_FindFirstInt.c
 // Win10
 // g++ 11.2.0
 // 
-// A 
+// A program that finds the first instance of an int in an array 
 //
 
 #include <stdio.h>
-int * FindFirstInt(const int *ptr, size_t count, int value)
+int *FindFirstInt(const int *ptr, size_t count, int value)
 {
-    for (int ind = 0; ind < count; ind++)
+    //Loop through the elements of the array
+    for (int ind = 0; ind < (int)count; ind++) 
     {
+        //check if it's the one we want
         if (ptr[ind] == value)
         {
-            return ptr[ind];
+            return (int *)(ptr + ind);
         }
     }
+    //return null if we don't find anything
     return NULL;
 }
